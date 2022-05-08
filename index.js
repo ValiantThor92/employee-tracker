453afd9240cf9031f85ec29737666920567f866a
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-08 13:39:47 -0700

    add require statements to server.js and db connection function

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer");
+const db = require("./db/connection.js");
+const console = require("console.table");
+
+// Connect to the database
+db.connect((err) => {
+  if (err) throw err;
+  userOptions();
+});
+
+
