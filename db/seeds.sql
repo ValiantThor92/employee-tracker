7e4fc27dc2362fbed806415fc26458f49c812534
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-09 10:36:22 -0700

    Add department roles w/ salary and department id numbers

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,14 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
+  ('Sales Manager', 100000, 1),
+  ('Sales Relations', 75000, 1),
+  ('Finance Manager', 100000, 2),
+  ('Accountant', 85000, 2),
+  ('Human Resources Manager', 100000, 3),
+  ('Human Resources Outreach Specialist', 85000, 3),
+  ('Technologies Manager', 125000, 4),
+  ('Technologies Engineer', 110000, 4),
+  ('Production Foreman', 65000, 5),
+  ('Production Staff', 45000, 5);
+
